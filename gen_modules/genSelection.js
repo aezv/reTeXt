e1b907996dcf30a8e533b967eabe1c4889681891
aezv <aezv.global@gmail.com>
2020-12-12 06:44:28 +0300

    Упрощение скрипта скрещивания поколений для работы нового скрипта сравнения для связных пространств

--- gen_modules/genSelection.js
+++ gen_modules/genSelection.js
@@ -48,57 +48,31 @@ function mergerR(objL, objR) {
     return objR + objL;
 }
 
-function divisionU(objU, objD) {
-    return '\\frac{' + objU + '}{' + objD + '}';
+function divisionL(objL, objR) {
+    return '\\frac{' + objL + '}{' + objR + '}';
 }
 
-function divisionD(objU, objD) {
-    return '\\frac{' + objD + '}{' + objU + '}';
+function divisionR(objL, objR) {
+    return '\\frac{' + objR + '}{' + objL + '}';
 }
 
-let selectionArrayFuncHorizontal = [ //1.14
+let selectionArrayFunc = [
     unificationL,
     unificationR,
     mergerL,
-    mergerR
+    mergerR,
+    divisionR,
+    divisionL
 ];
 
-let selectionArrayFuncVertical = [
-    divisionU,
-    divisionD
-];
-
-function selection(genObjectTexLeft, genObjectTexRight, genProcessingArea) {
+function selection(genObjectTexLeft, genObjectTexRight) {
     let object = new genObject();
-
-    for (let i = 0; i < genProcessingArea.arrayIndex.length; i++) {
-        if (genProcessingArea.arrayPosition[i].horizontal == 'left') {
-            for (let func = 0; func < selectionArrayFuncHorizontal.length; func += 2) {
-                object.tex.push(selectionArrayFuncHorizontal[func](genObjectTexLeft[genProcessingArea.arrayIndex[i][0]], genObjectTexRight[genProcessingArea.arrayIndex[i][1]]));
-                object.areas.push(genProcessingArea.arrayAreas[i]);
-            }
-        }
-        else {
-            for (let func = 1; func < selectionArrayFuncHorizontal.length; func += 2) {
-                object.tex.push(selectionArrayFuncHorizontal[func](genObjectTexLeft[genProcessingArea.arrayIndex[i][0]], genObjectTexRight[genProcessingArea.arrayIndex[i][1]]));
-                object.areas.push(genProcessingArea.arrayAreas[i]);
-            }
-        }
-
-        if (genProcessingArea.arrayPosition[i].vertical == 'up') {
-            for (let func = 0; func < selectionArrayFuncVertical.length; func += 2) {
-                object.tex.push(selectionArrayFuncVertical[func](genObjectTexLeft[genProcessingArea.arrayIndex[i][0]], genObjectTexRight[genProcessingArea.arrayIndex[i][1]]));
-                object.areas.push(genProcessingArea.arrayAreas[i]);
-            }
-        }
-        else {
-            for (let func = 1; func < selectionArrayFuncVertical.length; func += 2) {
-                object.tex.push(selectionArrayFuncVertical[func](genObjectTexLeft[genProcessingArea.arrayIndex[i][0]], genObjectTexRight[genProcessingArea.arrayIndex[i][1]]));
-                object.areas.push(genProcessingArea.arrayAreas[i]);
-            }
+    for (let iFunc = 0; iFunc < selectionArrayFunc.length; iFunc++) {
+        for (let i = 0; i < genObjectTexLeft.length; i++) {
+            for (let j = 0; j < genObjectTexRight.length; j++)
+                object.tex.push(selectionArrayFunc[iFunc](genObjectTexLeft[i], genObjectTexRight[j]));
         }
     }
-
     return object;
 }
 
