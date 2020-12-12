2bea8262696e9fcad261e271d673b3b8160a2695
aezv <aezv.global@gmail.com>
2020-12-13 01:18:00 +0300

    Преобразование скрипта генетического алгоритма в функцию

--- genAlgorithm.js
+++ genAlgorithm.js
@@ -7,100 +7,111 @@ const searchSymbols = require('./gen_modules/genSearchSymbols').searchSymbols;
 const genFilter = require('./gen_modules/genFilter').genFilter;
 const genProcessingArea = require('./gen_modules/genProcessingArea').processingArea;
 
-let comparisonBuffer = new Array();
-let genObjects = new Array();
+function genAlgorithm(binMatrixesBlock, callback) {
+    let comparisonBuffer = new Array();
+    let genObjects = new Array();
 
-let processHandlerObject = {
-    valueInternal: 0,
-    valueListener: function (val) { },
-    set value(val) {
-        this.valueInternal = val;
-        this.valueListener(val);
-    },
-    get value() {
-        return this.valueInternal;
-    },
-    listener: function (listener) {
-        this.valueListener = listener;
-    }
-}
-
-//**************************************
-
-let count = 0;
-let offset = 1;
-processHandlerObject.listener(function (val) {
-    let boolGen = true;
-    if (comparisonBuffer.length < val)
-        comparisonBuffer[val - offset] = new Array();
-    for (let i = 0; i < comparisonBuffer[val - offset].length; i++) {
-        if (comparisonBuffer[val - offset][i] == count) {
-            boolGen = false;
-            break;
+    let processHandlerObject = {
+        valueInternal: 0,
+        valueListener: function (val) { },
+        set value(val) {
+            this.valueInternal = val;
+            this.valueListener(val);
+        },
+        get value() {
+            return this.valueInternal;
+        },
+        listener: function (listener) {
+            this.valueListener = listener;
         }
     }
-    if (genObjects[val - 1].tex.length && boolGen) {
-        console.log('**************************************************');
-        console.log('Поколение ' + val + ' -> построение');
-        console.log('Левое поколение: ' + count);
-        console.log('Правое поколение: ' + (val - offset));
-        comparisonBuffer[val - offset].push(count);
 
-        genObjects[val] = new genObject();
-        genObjects[val] = selection(genObjects[count].tex, genObjects[val - offset].tex);
-        console.log('Поколение ' + val + ' -> фильтрация');
-        genObjects[val] = genFilter(genObjects, val);
-        console.log('Поколение ' + val + ' -> количество объектов ' + genObjects[val].tex.length);
-        console.log('Поколение ' + val + ' -> компиляция');
+    //**************************************
 
-        compileBin(genObjects[val].tex, function (matrixes) {
-            console.log('Поколение ' + val + ' -> сравнение');
-            genObjects[val] = genComparison(originalMatrix, matrixes, genObjects[val].tex);
-            console.log('Поколение ' + val + ' -> количество объектов прошедших отбор ' + genObjects[val].tex.length);
-            console.log('Поколение ' + val + ' -> объекты ');
-            console.log(genObjects[val].tex);
-            if (genObjects[val].tex.length) {
-                count = val;
-                offset = 1;
-                console.log('Поколение ' + val + ' -> результат: точность ' + genObjects[val].maxPrecision.value + ', формула ' + genObjects[val].tex[genObjects[val].maxPrecision.index]);
+    let count = 0;
+    let offset = 1;
+    processHandlerObject.listener(function (val) {
+        let boolGen = true;
+        if (comparisonBuffer.length < val)
+            comparisonBuffer[val - offset] = new Array();
+        for (let i = 0; i < comparisonBuffer[val - offset].length; i++) {
+            if (comparisonBuffer[val - offset][i] == count) {
+                boolGen = false;
+                break;
+            }
+        }
+        if (genObjects[val - 1].tex.length && boolGen) {
+            console.log('**************************************************');
+            console.log('Поколение ' + val + ' -> построение');
+            console.log('Левое поколение: ' + count);
+            console.log('Правое поколение: ' + (val - offset));
+            comparisonBuffer[val - offset].push(count);
+
+            genObjects[val] = new genObject();
+            genObjects[val] = selection(genObjects[count].tex, genObjects[val - offset].tex);
+            console.log('Поколение ' + val + ' -> фильтрация');
+            genObjects[val] = genFilter(genObjects, val);
+            console.log('Поколение ' + val + ' -> количество объектов ' + genObjects[val].tex.length);
+            console.log('Поколение ' + val + ' -> компиляция');
+
+            compileBin(genObjects[val].tex, function (matrixes) {
+                console.log('Поколение ' + val + ' -> сравнение');
+                genObjects[val] = genComparison(originalMatrix, matrixes, genObjects[val].tex);
+                console.log('Поколение ' + val + ' -> количество объектов прошедших отбор ' + genObjects[val].tex.length);
+                console.log('Поколение ' + val + ' -> объекты ');
+                console.log(genObjects[val].tex);
+                if (genObjects[val].tex.length) {
+                    count = val;
+                    offset = 1;
+                    console.log('Поколение ' + val + ' -> результат: точность ' + genObjects[val].maxPrecision.value + ', формула ' + genObjects[val].tex[genObjects[val].maxPrecision.index]);
+                }
+                else
+                    console.log('Поколение ' + val + ' -> результат: пустое поколение');
+                if (genObjects[val].maxPrecision.value < 0.995)
+                    processHandlerObject.value++;
+                else {
+                    console.log('Результат -> ' + genObjects[val].tex[genObjects[val].maxPrecision.index]);
+                    callback(genObjects[val].tex[genObjects[val].maxPrecision.index]);
+                }
+            });
+        }
+        else if (genObjects[val - 1].tex.length && !boolGen) {
+            if (count == 0) {
+                offset++;
+                count = val - offset;
             }
             else
-                console.log('Поколение ' + val + ' -> результат: пустое поколение');
-            if (genObjects[val].maxPrecision.value < 0.995)
-                processHandlerObject.value++;
+                count--;
+            processHandlerObject.value += 0;
+        }
+        else {
+            genObjects.splice(genObjects.length - 1, 1);
+            if (count == 0) {
+                offset++;
+                count = val - offset - 1;
+            }
             else
-                console.log('Результат -> ' + genObjects[val].tex[genObjects[val].maxPrecision.index]);
-        });
-    }
-    else if (genObjects[val - 1].tex.length && !boolGen) {
-        if (count == 0) {
-            offset++;
-            count = val - offset;
+                count--;
+            processHandlerObject.value--;
         }
-        else
-            count--;
-        processHandlerObject.value += 0;
-    }
-    else {
-        genObjects.splice(genObjects.length - 1, 1);
-        if (count == 0) {
-            offset++;
-            count = val - offset - 1;
-        }
-        else
-            count--;
-        processHandlerObject.value--;
-    }
-});
-//**************************************
+    });
+    //**************************************
 
-pngToBin(process.argv[2]).then(matrix => {
-    originalMatrix = matrix[0];
+
+    originalMatrix = binMatrixesBlock;
     searchSymbols(originalMatrix, function (error, rGenObject) {
         genObjects[0] = rGenObject;
         console.log('Подбор символов завершен');
         console.log('Найдены символы: ' + genObjects[0].tex);
-        processHandlerObject.value++;
+        if (genObjects[0].maxPrecision.value <= 0.995) {
+            processHandlerObject.value++;
+        }
+        else {
+            console.log('Результат -> ' + genObjects[0].tex[genObjects[0].maxPrecision.index]);
+            callback(genObjects[0].tex[genObjects[0].maxPrecision.index]);
+        }
     });
-});
 
+}
+
+module.exports = genAlgorithm;
