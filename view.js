ca2672c0cb27bd6ffb99ca77d9680e48b9722379
aezv <aezv.global@gmail.com>
2020-12-12 06:48:11 +0300

    Обновление скрипта просмотра матрицы пикселей

--- view.js
+++ view.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 var tex = JSON.parse(fs.readFileSync('./gen_modules/symbols.json'));
-var bin = JSON.parse(fs.readFileSync('./gen_modules/bin.json'));
+//var bin = JSON.parse(fs.readFileSync('./gen_modules/bin.json'));
 var temp = JSON.parse(fs.readFileSync('./temp.json'));
 
 if (process.argv[2] == '--all') {
@@ -36,22 +36,20 @@ else if (process.argv[2] == '--last') {
     }
 }
 else if (process.argv[2] == '--temp') {
-    let jsonRevers = temp;
+    let json = temp;
 
-    let json = new Array();
-    for (let y = 0; y < jsonRevers[0].length; y++) {
-        let temp = new Array();
-        for (let x = 0; x < jsonRevers.length; x++)
-            temp.push(jsonRevers[x][y]);
-        json.push(temp);
-    }
-
-    for (let x = 0; x < json.length; x++) {
-        let str = '';
-        for (let y = 0; y < json[x].length; y++) {
-            str += json[x][y];
+    for (let iCount = 0; iCount < json.length; iCount++) {
+        console.log('БЛОК');
+        for (let i = 0; i < json[iCount].length; i++) {
+            for (let x = 0; x < json[iCount][i].length; x++) {
+                let str = '';
+                for (let y = 0; y < json[iCount][i][x].length; y++) {
+                    str += json[iCount][i][x][y];
+                }
+                console.log(str);
+            }
+            console.log('**********');
         }
-        console.log(str);
     }
 }
 else if (process.argv[2] == '--log') {
