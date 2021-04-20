630ec6ece7919dcff66cca466dc1ca073ee891d1
aezv <aezv.global@gmail.com>
2021-04-20 15:42:59 +0300

    [C] Фикс метода масштабирования

--- source/methods_image.c
+++ source/methods_image.c
@@ -11,14 +11,14 @@ unsigned char *img_binarization(unsigned char *img, int width, int heigth, unsig
 unsigned char *img_interpolation(unsigned char *img, int width, int height, int new_width, int new_height)
 {
     unsigned char *new_img = malloc(sizeof(unsigned char) * new_width * new_height);
-    float scale_y = (float)height / (float)new_height;
-    float scale_x = (float)width / (float)new_width;
+    float scale_y = (float)(height - 1) / (float)(new_height - 1);
+    float scale_x = (float)(width - 1) / (float)(new_width - 1);
     for (int y = 0; y < new_height; y++)
     {
-        int interpolation_y = (int)round((float)y * scale_y - 0.25) < height ? (int)round((float)y * scale_y - 0.25) : height - 1;
+        int interpolation_y = (int)round((float)y * scale_y);
         for (int x = 0; x < new_width; x++)
         {
-            int interpolation_x = (int)round((float)x * scale_x - 0.25) < width ? (int)round((float)x * scale_x - 0.25) : width - 1;
+            int interpolation_x = (int)round((float)x * scale_x);
             new_img[y * new_width + x] = img[interpolation_y * width + interpolation_x];
         }
     }
